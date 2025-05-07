6be12eaae737323fd374c84a0e049ef6578ced68
Pritesh Tupe <mail@priteshtupe.com>
2025-05-07 19:42:47 +0530

    Add sitemap, robots, compression and prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from "astro/config"
+
+import sitemap from "@astrojs/sitemap"
+import robotsTxt from "astro-robots-txt"
+import compressor from "astro-compressor"
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: {
+    prefetchAll: true,
+  },
   site: "https://www.priteshtupe.com",
   devToolbar: { enabled: false },
-});
+  integrations: [sitemap(), robotsTxt(), compressor()],
+})
